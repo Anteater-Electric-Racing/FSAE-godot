6130df20554951b0d0f0a192a6243f6fa45ef833
L4852 <122051988+L4852@users.noreply.github.com>
2026-01-24 15:04:36 -0800

    Basic car controls completed
    WASD controls added (no reverse) but persistent issues with car turning over when steering at higher speed

--- vehicle_body_3d.gd
+++ vehicle_body_3d.gd
@@ -13,24 +13,29 @@ func _process(delta: float) -> void:
 func deg_to_rad(deg):
 	return deg * PI / 180
 	
+func rad_to_deg(rad):
+	return rad * 180 / PI
+	
 func _physics_process(delta: float) -> void:
 	var steer = 0.0
 	var throttle = 0.0
+	var braking = 0.0
 	
-	const MAX_POWER = 900.0
-	const ACCELERATION_STEP = 20.0
-	const BRAKING_STEP = 50.0
+	const MAX_POWER = 50.0
+	const ACCELERATION_STEP = 5.0
+	const BRAKING_STEP = 1.0
 	const BASE_DECEL = 5.0
-	const STEER_STEP = 10.0
-	const MAX_STEERING_ANGLE = 30.0
+	const MAX_BRAKING = 15.0
+	const STEER_STEP = 0.5
+	const MAX_STEERING_ANGLE = 15.0
 	
 	
 	# Map inputs to parameters
 	if Input.is_action_pressed("accelerate"):
 		throttle = ACCELERATION_STEP
 	elif Input.is_action_pressed("decelerate"):
-		throttle = -BRAKING_STEP;
-		
+		braking = BRAKING_STEP;
+	
 	if Input.is_action_pressed("steer_left"):
 		steer = STEER_STEP
 	elif Input.is_action_pressed("steer_right"):
@@ -43,21 +48,27 @@ func _physics_process(delta: float) -> void:
 			# Apply throttle and steering parameters
 			if wheel.use_as_traction:
 				if throttle > 0 and engine_force + throttle <= MAX_POWER:
+					#wheel.brake = 0.0
 					wheel.engine_force += throttle
-				elif throttle < 0 and engine_force - throttle >= 0:
-					wheel.engine_force -= throttle
+				elif braking > 0 and wheel.brake + braking <= MAX_BRAKING:
+					wheel.brake += braking
+					wheel.engine_force = 0.0
 				else:
-					wheel.engine_force = 0.0;
+					# Apply natural deceleration parameter
+					wheel.engine_force = 0.0
+					wheel.brake = BASE_DECEL
+					
+				
 				
 			elif wheel.use_as_steering:
-				if steer > 0 and wheel.steering + steer <= MAX_STEERING_ANGLE:
-					wheel.steering += steer
-				elif steer < 0 and wheel.steering - steer >= 0:
-					wheel.steering -= steer
-				else:
-					wheel.steering = 0.0;
-			
-			# Apply natural deceleration parameter
-			if throttle == 0.0:
-				wheel.brake = BASE_DECEL
+				var steer_deg = rad_to_deg(wheel.steering)
+				var steer_rad = deg_to_rad(steer)
+				
+				if wheel.steering > PI:
+					wheel.steering /= PI
+				
+				if steer > 0 and steer_deg + steer <= MAX_STEERING_ANGLE:
+					wheel.steering += steer_rad
+				elif steer < 0 and steer_deg + steer >= -MAX_STEERING_ANGLE:
+					wheel.steering += steer_rad
 			
