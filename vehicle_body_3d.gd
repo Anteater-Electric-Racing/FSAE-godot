b725de570c42a14738aaad25f0fd79ad60958ddf
L4852 <122051988+L4852@users.noreply.github.com>
2026-01-24 16:48:22 -0800

    Added speed debug display, fixed car center of mass
    Center of mass / car handling fixed, added debug speed display

--- vehicle_body_3d.gd
+++ vehicle_body_3d.gd
@@ -1,5 +1,8 @@
 extends VehicleBody3D
 
+const DISPLAY_DEBUG_SPEED = true
+
+var previousDisplayedSpeed = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,6 +19,9 @@ func deg_to_rad(deg):
 func rad_to_deg(rad):
 	return rad * 180 / PI
 	
+func kph_to_mph(kph):
+	return kph / 1.609
+	
 func _physics_process(delta: float) -> void:
 	var steer = 0.0
 	var throttle = 0.0
@@ -27,10 +33,11 @@ func _physics_process(delta: float) -> void:
 	const BASE_DECEL = 5.0
 	const MAX_BRAKING = 15.0
 	const STEER_STEP = 0.5
-	const MAX_STEERING_ANGLE = 15.0
+	const MAX_STEERING_ANGLE = 20.0
+	const MAX_VEHICLE_SPEED_MPH = 40.0
 	
 	
-	# Map inputs to parameters
+	# Map inputs to parameter deltas
 	if Input.is_action_pressed("accelerate"):
 		throttle = ACCELERATION_STEP
 	elif Input.is_action_pressed("decelerate"):
@@ -41,13 +48,30 @@ func _physics_process(delta: float) -> void:
 	elif Input.is_action_pressed("steer_right"):
 		steer = -STEER_STEP
 		
+	# Debug printing for vehicle speed
+	
+	if DISPLAY_DEBUG_SPEED:
+		var velocity_mps = linear_velocity.length()
+		
+		var velocity_kph = int(round(velocity_mps * 3.6))
+		
+		var velocity_mph = int(round(velocity_kph / 1.609))
+		
+		if velocity_kph != previousDisplayedSpeed:
+			var printString = "VEHICLE SPEED | " + str(velocity_kph) + " km/h" + " (" + str(velocity_mph) + " mph)"
+			print(printString)
+			
+		previousDisplayedSpeed = velocity_kph
+		
+	# Update vehicle physics based on deltas
+	
 	for object in get_children():
 		if object is VehicleWheel3D:
 			var wheel = object
 			
 			# Apply throttle and steering parameters
 			if wheel.use_as_traction:
-				if throttle > 0 and engine_force + throttle <= MAX_POWER:
+				if throttle > 0 and engine_force + throttle <= MAX_POWER and int(round(linear_velocity.length() * 3.6 / 1.609)) < MAX_VEHICLE_SPEED_MPH:
 					#wheel.brake = 0.0
 					wheel.engine_force += throttle
 				elif braking > 0 and wheel.brake + braking <= MAX_BRAKING:
